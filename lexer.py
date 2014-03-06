fcc388f18ceb23228e2981e9b07df231312b8a40
juan <juan.corrales@cerok.com>
2014-03-06 13:35:19 -0500

    Proyecto de compiladores 114

---
+++ lexer.py
@@ -0,0 +1,64 @@
+# ------------------------------------------------------------
+# calclex.py
+#
+# tokenizer for a simple expression evaluator for
+# numbers and +,-,*,/
+# ------------------------------------------------------------
+import ply.lex as lex
+
+#Lista de palabras reservadas
+
+
+# List of token names.   This is always required
+tokens = (
+   'NUMBER',
+   'PLUS',
+   'MINUS',
+   'TIMES',
+   'DIVIDE',
+   'LPAREN',
+   'RPAREN',
+)
+
+# Regular expression rules for simple tokens
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+
+# A regular expression rule with some action code
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):    
+    print "Illegal character '%s'" % t.value[0] + "line"
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+def leertxt():
+    
+    with open('codigopascal.txt') as arch:
+        data = arch.read()
+        lexer.input(data)
+    while True:
+        tok = lexer.token()
+        if not tok: break      # No more input
+        print tok.type, tok.value, tok.lineno, tok.lexpos
+
+
+leertxt()
